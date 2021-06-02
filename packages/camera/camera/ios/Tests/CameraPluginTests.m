fe2e1326266b9ef4270727480a1fadd064c2f3a4
Rene Floor <rene@baseflow.com>
2021-06-02 17:58:47 +0200

    add failing test

--- packages/camera/camera/ios/Tests/CameraPluginTests.m
+++ packages/camera/camera/ios/Tests/CameraPluginTests.m
@@ -15,6 +15,7 @@
   // Make sure this plugin supports modules. See https://github.com/flutter/flutter/issues/41007.
   // If not already present, add this line to the podspec:
   // s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES' }
+  XCTFail("test");
 }
 
 @end
